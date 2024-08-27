d552b17f9edf7649191bf861c15f9aeada513142
Klaus Jørgensen <knj@sonarapp.com>
2024-08-28 00:57:18 +0200

    added personnel messages

---
+++ messages/personnel.go
@@ -0,0 +1,42 @@
+package messages
+
+import "github.com/nathejk/shared-go/types"
+
+// nathejk:user.updated
+type NathejkUserUpdated struct {
+	UserID   types.UserID      `json:"userId"`
+	Name     string            `json:"name"`
+	Phone    types.PhoneNumber `json:"phone"`
+	HqAccess bool              `json:"hqAccess"`
+	Group    string            `json:"group"`
+}
+
+// nathejk:user.deleted
+type NathejkUserDeleted struct {
+	UserID types.UserID `json:"userId"`
+}
+
+// nathejk:personnel.updated
+type NathejkPersonnelUpdated struct {
+	UserID     types.UserID      `json:"userId"`
+	Name       string            `json:"name"`
+	Phone      types.PhoneNumber `json:"phone"`
+	Email      types.Email       `json:"email"`
+	HqAccess   bool              `json:"hqAccess"`
+	Pincode    string            `json:"pincode"`
+	Department string            `json:"department"`
+	MedlemNr   string            `json:"medlemnr"`
+	Corps      types.CorpsSlug   `json:"corps"`
+	Diet       string            `json:"diet"`
+}
+
+type NathejkPersonnelCreated struct {
+	UserID  types.UserID      `json:"userId"`
+	Phone   types.PhoneNumber `json:"phone"`
+	Pincode string            `json:"pincode"`
+}
+
+// nathejk:personnel.deleted
+type NathejkPersonnelDeleted struct {
+	UserID types.UserID `json:"userId"`
+}

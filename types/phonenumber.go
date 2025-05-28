4773c426c85479747aabad07e77376ce88a7b4bc
Klaus Jørgensen <knj@sonarapp.com>
2025-05-28 14:21:23 +0200

    get international version of phone number

--- types/phonenumber.go
+++ types/phonenumber.go
@@ -14,3 +14,13 @@ func (v PhoneNumber) Normalize() string {
 	re := regexp.MustCompile("[0-9]+")
 	return strings.Join(re.FindAllString(string(v), -1), "")
 }
+func (v PhoneNumber) CountryCode() string {
+	if len(v.Normalize()) == 8 {
+		return "45"
+	}
+	return ""
+}
+
+func (v PhoneNumber) InternationalNumber() string {
+	return v.CountryCode() + v.Normalize()
+}

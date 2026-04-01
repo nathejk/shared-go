3c25c2563923c304d73b691376c35bbb3a3f6644
Klaus Jørgensen <knj@sonarapp.com>
2026-04-01 14:48:23 +0200

    fixed compile warnings

--- messages/monolith.go
+++ messages/monolith.go
@@ -7,43 +7,43 @@ type MonolithNathejkTeam struct {
 	Entity struct {
 		ID                   types.TeamID         `json:"id"`
 		RemoteID             string               `json:"remoteId"`
-		CreatedUts           types.UnixtimeString `json"createdUts"`
-		VerifiedUts          types.UnixtimeString `json"verifiedUts"`
-		OpenedUts            types.UnixtimeString `json"openedUts"`
-		FinishedUts          types.UnixtimeString `json"finishedUts"`
-		CanceledUts          types.UnixtimeString `json"canceledUts"`
-		DeletedUts           types.UnixtimeString `json"deletedUts"`
-		LastModifyUts        types.UnixtimeString `json"lastModifyUts"`
-		StartUts             types.UnixtimeString `json"startUts"`
-		FinishUts            types.UnixtimeString `json"finishUts"`
-		SignupStatusTypeName string               `json"signupStatusTypeName"`
-		TypeName             types.TeamType       `json"typeName"`
-		ParentTeamID         types.TeamID         `json"parentTeamId"`
-		IP                   string               `json"ip"`
-		Title                string               `json"title"`
-		Gruppe               string               `json"gruppe"`
-		Division             string               `json"division"`
-		Korps                string               `json"korps"`
-		ContactTitle         string               `json"contactTitle"`
-		ContactAddress       string               `json"contactAddress"`
-		ContactPostalCode    string               `json"contactPostalCode"`
-		ContactMail          types.EmailAddress   `json"contactMail"`
-		ContactPhone         types.PhoneNumber    `json"contactPhone"`
-		ContactRole          string               `json"contactRole"`
-		TeamNumber           string               `json"teamNumber"`
-		LigaNumber           string               `json"ligaNumber"`
-		LigaNumberVerified   *string              `json"ligaNumberVerified"`
-		LokNumber            string               `json"lokNumber"`
-		MemberCount          string               `json"memberCount"`
-		PaidMemberCount      string               `json"paidMemberCount"`
-		Paid                 string               `json"paid"`
-		PaidStatus           *string              `json"paidStatus"`
-		Remark               string               `json"remark"`
-		PhotoID              string               `json"photoId"`
-		PhotoUts             types.UnixtimeString `json"photoUts"`
-		AdvSpejdNightCount   string               `json"advSpejdNightCount"`
-		ArrivalName          string               `json"arrivalName"`
-		CheckedAtStart       string               `json"checkedAtStart"`
+		CreatedUts           types.UnixtimeString `json:"createdUts"`
+		VerifiedUts          types.UnixtimeString `json:"verifiedUts"`
+		OpenedUts            types.UnixtimeString `json:"openedUts"`
+		FinishedUts          types.UnixtimeString `json:"finishedUts"`
+		CanceledUts          types.UnixtimeString `json:"canceledUts"`
+		DeletedUts           types.UnixtimeString `json:"deletedUts"`
+		LastModifyUts        types.UnixtimeString `json:"lastModifyUts"`
+		StartUts             types.UnixtimeString `json:"startUts"`
+		FinishUts            types.UnixtimeString `json:"finishUts"`
+		SignupStatusTypeName string               `json:"signupStatusTypeName"`
+		TypeName             types.TeamType       `json:"typeName"`
+		ParentTeamID         types.TeamID         `json:"parentTeamId"`
+		IP                   string               `json:"ip"`
+		Title                string               `json:"title"`
+		Gruppe               string               `json:"gruppe"`
+		Division             string               `json:"division"`
+		Korps                string               `json:"korps"`
+		ContactTitle         string               `json:"contactTitle"`
+		ContactAddress       string               `json:"contactAddress"`
+		ContactPostalCode    string               `json:"contactPostalCode"`
+		ContactMail          types.EmailAddress   `json:"contactMail"`
+		ContactPhone         types.PhoneNumber    `json:"contactPhone"`
+		ContactRole          string               `json:"contactRole"`
+		TeamNumber           string               `json:"teamNumber"`
+		LigaNumber           string               `json:"ligaNumber"`
+		LigaNumberVerified   *string              `json:"ligaNumberVerified"`
+		LokNumber            string               `json:"lokNumber"`
+		MemberCount          string               `json:"memberCount"`
+		PaidMemberCount      string               `json:"paidMemberCount"`
+		Paid                 string               `json:"paid"`
+		PaidStatus           *string              `json:"paidStatus"`
+		Remark               string               `json:"remark"`
+		PhotoID              string               `json:"photoId"`
+		PhotoUts             types.UnixtimeString `json:"photoUts"`
+		AdvSpejdNightCount   string               `json:"advSpejdNightCount"`
+		ArrivalName          string               `json:"arrivalName"`
+		CheckedAtStart       string               `json:"checkedAtStart"`
 	} `json:"entity"`
 	Changed []string `json:"changed"`
 	Sql     struct {
@@ -57,7 +57,7 @@ type MonolithNathejkPayment struct {
 		ID      string               `json:"id"`
 		OrderID string               `json:"orderId"`
 		TeamID  types.TeamID         `json:"teamId"`
-		Uts     types.UnixtimeString `json;"uts"`
+		Uts     types.UnixtimeString `json:"uts"`
 		Amount  string               `json:"amount"`
 	} `json:"entity"`
 	Changed []string `json:"changed"`
@@ -71,7 +71,7 @@ type MonolithNathejkMember struct {
 	Entity struct {
 		ID              types.MemberID       `json:"id"`
 		TeamID          types.TeamID         `json:"teamId"`
-		Number          string               `json;"number"`
+		Number          string               `json:"number"`
 		CreatedUts      types.UnixtimeString `json:"createdUts"`
 		PausedUts       types.UnixtimeString `json:"pausedUts"`
 		InhqUts         types.UnixtimeString `json:"inhqUts"`

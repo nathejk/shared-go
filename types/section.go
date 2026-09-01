51cf6d2fdf073043fc39b111de4cf43dc9dc9c5f
Klaus Jørgensen <knj@sonarapp.com>
2026-09-01 10:28:54 +0200

    Refine section doc comments

--- types/section.go
+++ types/section.go
@@ -10,7 +10,7 @@ const (
 	SectionTypeBandit SectionType = "bandit"
 	// SectionTypeActor is the section of gøglere putting on acts for the patrols.
 	SectionTypeActor SectionType = "gøgler"
-	// SectionTypePost is the section manning a fixed post along the course.
+	// SectionTypePost is the section manning a fixed checkpoint along the course.
 	SectionTypePost SectionType = "post"
 	// SectionTypeCrew is the general crew section, used for everyone not
 	// covered by a more specific section type.
@@ -26,7 +26,7 @@ const (
 	// SectionFlagSelfAssignable allows a crew member to join the section on
 	// their own, without being assigned to it by someone else.
 	SectionFlagSelfAssignable SectionFlag = "selfassignable"
-	// SectionFlagSosAssignable allows SOS to assign members to the section.
+	// SectionFlagSosAssignable allows Trailside assistence (Nødtelefonen) to assign tasks to the section.
 	SectionFlagSosAssignable SectionFlag = "sosassignable"
 	// SectionFlagDispatchable allows members of the section to be dispatched
 	// on tasks.
